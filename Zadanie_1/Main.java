908da13b1459979d87fca34f0029e5d050e4ea35
TomaszKadziela <kadzito_aehit@students.vizja.pl>
2024-04-06 09:36:01 +0200

    zadanie 1

---
+++ Zadanie_1/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Point point = new Point(3.5, 4.5);
+        System.out.println("Punkt ma współrzędne: x = " + point.getX() + ", y = " + point.getY());
+    }
+}
